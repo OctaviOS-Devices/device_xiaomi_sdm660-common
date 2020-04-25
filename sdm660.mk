b74c4111cace259241572a376aa76b47a67de5f9
TheHitMan7 <krtik.vrma@gmail.com>
2020-04-25 13:59:50 +0530

    sdm660-common: Add APN config
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -137,6 +137,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # AuthSecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0-service
