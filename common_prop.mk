3d86709b92d5135121ac9b6d02d9dbe45e5df768
smokey18 <kmboplays@gmail.com>
2020-12-03 22:46:04 +0000

    sdm660-common: Enable Dirac audio effect
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- common_prop.mk
+++ common_prop.mk
@@ -368,3 +368,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.sdha_apps_bg_max=64 \
     ro.config.sdha_apps_bg_min=8
 
+# Dirac algo tsx 9/12
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dirac.acs.controller=qem \
+    persist.dirac.acs.storeSettings=1 \
+    persist.dirac.acs.ignore_error=1
+
+# Dirac headset effect
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.soundfx.dirac=true \
+    persist.audio.dirac.speaker=true
+
